b0f81193f687f980f91a752da772c24b7ff8281f
Hrutwikbaraskar <117747747+hrutb@users.noreply.github.com>
2026-07-04 14:56:06 +0530

    promise.all() is used

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,13 @@ function snackbar(msg,icon){
        })
 }
 
-
+const makeApiCall = async(ApiUrl)=>{  
+     const res = await fetch(ApiUrl);
+     if(!res.ok){ 
+          throw new Error(res);
+     } 
+       return await res.json();
+}
 
 
 async function getProfile(){
@@ -26,27 +32,22 @@ async function getProfile(){
    spinner.classList.remove('d-none');
 
    try{ 
-     //we fetched data by using github user profile api 
-    
-     //fetch():= it is method of window interface....it starts the processs of fetching a resourece from  the network 
-      //fetch() method returns the promise  that is fullfilled once  response is available
-
-       const result = await fetch(`https://api.github.com/users/${userName}`); 
+   
+       const userUrl = `https://api.github.com/users/${userName}`;
+         const User_Repo_url =`https://api.github.com/users/${userName}/repos?sorted=created`;
+                      
+               const result =[makeApiCall(userUrl), makeApiCall(User_Repo_url)];
+           
+                         
               console.log(userName);
-              
-
-       //ok is read only proprty of response interface it is boolean stating whether reponse was successfull or not 
+           
      if(!result.ok){ 
          snackbar('User not found', 'error');
      }  
-
-     //now we have to parse row HTTP data into javascript object
           const  data = await result.json();
- 
        console.log(data);
-       
-     
-     // profileTemplate();
+
+       // profileTemplate();
       profileContainer.innerHTML = `<div class="col-md-6">
                                         <div class="card">
                                             <div class="card-header d-flex justify-content-between align-items-center">
@@ -59,8 +60,8 @@ async function getProfile(){
                                                         <img src="${data.avatar_url}" alt="">
                                                     </figure>
                                                     <figcaption>
-                                                        <p>Followers: ${data.followers || '...'}</p>
-                                                        <p>Following: ${data.following || '...'}</p>
+                                                        <p>Followers: ${data.followers || 0}</p>
+                                                        <p>Following: ${data.following | 0}</p>
                                                         <p>Bio: ${data.bio || '...'}</p>
                                                         <p>public_Repos: ${data.public_repos || '...'} </p>
                                                         <p>Profile: <a href="${data.html_url || '...'}">Visit github</a></p>
@@ -73,11 +74,11 @@ async function getProfile(){
                                     </div>`
 
    }catch(err){ 
-        snackbar(err.msg, 'error')
+             snackbar(err.msg, 'error')
    }
    finally{ 
       spinner.classList.add('d-none'); 
-      searchForm.reset();;
+      searchForm.reset();
    }
 
 
@@ -85,7 +86,7 @@ async function getProfile(){
 
 } 
 
-
+getProfile();
 
 
 
